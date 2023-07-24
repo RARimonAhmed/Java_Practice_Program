a713b5108e09f1dccf208962ce0781bcf79e4952
RARimonAhmed <rayhanahmedrimon44@gmail.com>
2023-07-24 16:06:00 +0600

    Second practice is done

---
+++ Java_02/src/MyClass.java
@@ -0,0 +1,45 @@
+public class MyClass {
+    public static void main(String [] args){
+        System.out.println("Hello");
+        byte byte1=1,byte2=12;
+        short s1,s2;
+        long l1,l2;
+        int number1,number2,number3;
+        char c1,c2;
+        var v1 = 1;
+        float f1,f2;
+        double d1,d2;
+        String str1,str2;
+        s1 = 12;
+        s2 = 15000;
+        l1 = 1200000000;
+        l2 = 15000;
+        number1 = 15300;
+        number2 = 1200000000;
+        number3 = 15000;
+        f1 = 15000;
+        f2 = 1200000000;
+        d1 = 1500000000.00405;
+        d2 = 17000300567854646546516565484.145;
+        str1 = "This is string practice";
+        str2 = "So its working nice";
+        c1 = 'c';
+        c2 =  'b';
+        System.out.print("All byte numbers sum is ");
+        System.out.println(byte1+byte2);
+        System.out.print("All short numbers sum is ");
+        System.out.println(s1+s2);
+        System.out.print("All long numbers sum is ");
+        System.out.println(l1+l2);
+        System.out.print("All integer numbers sum is ");
+        System.out.println(number1+number2+number3);
+        System.out.print("All float numbers sum is ");
+        System.out.println(f1+f2);
+        System.out.print("All double numbers sum is ");
+        System.out.println(d1+d2);
+        System.out.print("String line is : ");
+        System.out.println(str1+" "+str2);
+        System.out.print("Char is : ");
+        System.out.println(c1+" "+c2);
+    }
+}
