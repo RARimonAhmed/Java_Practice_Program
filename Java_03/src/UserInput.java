ea74f5c1d3467da1ef0c39ac2ce3651df7af5293
RARimonAhmed <rayhanahmedrimon44@gmail.com>
2023-07-25 00:32:19 +0600

    Some changes this file

--- Java_03/src/UserInput.java
+++ Java_03/src/UserInput.java
@@ -1,31 +1,30 @@
 import java.util.Scanner;
 
 public class UserInput {
-    public static void main(String[] args) {
-        int number1, number2;
-        double doubleNum1, doubleNum2;
-        String fName, lName;
+    public static void main(String [] args){
+        int number1,number2;
+        double doubleNum1,doubleNum2;
+        String fName,lName;
+        var n = 1;
+        System.out.println(n);
         Scanner userInput = new Scanner(System.in);
-
         System.out.print("Please enter your first number : ");
-        number1 = Integer.parseInt(userInput.nextLine());
-
+        number1 = userInput.nextInt();
         System.out.print("Please enter your second number : ");
-        number2 = Integer.parseInt(userInput.nextLine());
+        number2 = userInput.nextInt();
         int result = number1 + number2;
-        System.out.println("Sum of " + number1 + " and " + number2 + " is " + result);
-
+        System.out.println("Sum of "+number1 +" and "+number2 +" is "+result);
         System.out.print("Please enter your third number : ");
-        doubleNum1 = Double.parseDouble(userInput.nextLine());
-
+        doubleNum1 = userInput.nextDouble();
         System.out.print("Please enter your fourth number : ");
-        doubleNum2 = Double.parseDouble(userInput.nextLine());
+        doubleNum2 = userInput.nextDouble();
         double doubleValueResult = doubleNum1 + doubleNum2;
-        System.out.println("Sum of " + doubleNum1 + " and " + doubleNum2 + " is " + doubleValueResult);
+        System.out.print("Sum of "+doubleNum1 +" and "+doubleNum2 +" is "+doubleValueResult);
+        userInput.nextLine();
         System.out.print("Please enter your first name : ");
         fName = userInput.nextLine();
-        System.out.print("Please enter your last name : ");
+        System.out.println("Please enter your last name : ");
         lName = userInput.nextLine();
-        System.out.println("Your full name is : " + fName + " " + lName);
+        System.out.println("Your full name is : "+fName+""+lName);
     }
 }
